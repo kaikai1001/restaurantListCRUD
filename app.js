b5b157b1b8a95616fb7a612156d56c0ae88340bb
kaiaki1001 <cccjjmm09@gmail.com>
2023-04-29 22:09:03 +0800

    feat: add create restaurant funtion

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 const express = require('express')
 const app = express()
 
+//啟用body-parser
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extented: true }))
+
 //啟用 Handlebars
 const exphbs = require('express-handlebars')
 
@@ -15,8 +19,7 @@ app.use(express.static('public'))
 const Restaurant = require('./models/restaurant')
 
 // 載入 mongoose
-const mongoose = require('mongoose') 
-// const restaurant = require('./models/restaurant')
+const mongoose = require('mongoose')
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -39,9 +42,22 @@ db.once('open', () => {
 
 // 設定首頁路由
 app.get('/', (req, res) => {
-  Restaurant.find() 
+  Restaurant.find()
     .lean()
-    .then(restaurants => res.render('index', { restaurants}))
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.log(error))
+})
+
+//新增餐廳的頁面
+app.get('/restaurants/new', (req, res) => {
+  res.render('new')
+})
+
+//新增餐廳
+app.post('/restaurants', (req, res) => {
+  const data = req.body // 從 req.body 拿出表單裡的資料
+  Restaurant.create(data)  // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
     .catch(error => console.log(error))
 })
 
