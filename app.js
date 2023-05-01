512e89e312c45c6d92e228a4e18475439fa92e43
kaiaki1001 <cccjjmm09@gmail.com>
2023-05-01 20:06:01 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -25,31 +25,9 @@ const routes = require('./routes')
 // 將 request 導入路由器
 app.use(routes)
 
-//載入 Restaurant model 
-const Restaurant = require('./models/restaurant')
 
-// 載入 mongoose
-const mongoose = require('mongoose')
+require('./config/mongoose')
 
-
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-//process.env.MONGODB_URI
-mongoose.connect('mongodb+srv://alpha:camp@cluster0.rdlqvcs.mongodb.net/restaurant-list?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 // 設定 port 3000
 app.listen(3000, () => {
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,13 +1,9 @@
-const mongoose = require('mongoose')
 const Restaurant = require('../restaurant')
 const restaurantList = require('../../restaurant.json').results
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+const db = require('../../config/mongoose')
+
 db.once('open', () => {
   console.log('mongodb connected!')
   Restaurant.create(restaurantList)
