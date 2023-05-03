c7d718e626991b638440da9604771e83b6160b3a
kaiaki1001 <cccjjmm09@gmail.com>
2023-05-03 14:09:20 +0800

    feat: update restaurant function(upadate)

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -37,11 +37,19 @@ router.get('/:id/edit', (req, res) => {
 //儲存編輯完的頁面
 router.put('/:id', (req, res) => {
   const id = req.params.id
-  const data = req.body
+  const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
   return Restaurant.findById(id)
     .then(restaurant => {
-      restaurant = data
-      return restaurant.save() //<----超奇怪
+      restaurant.name = name
+      restaurant.name_en = name_en
+      restaurant.category = category
+      restaurant.image = image
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.google_map = google_map
+      restaurant.rating = rating
+      restaurant.description = description
+      return restaurant.save()
     })
     .then(() => res.redirect(`/restaurants/${id}`))
     .catch(error => console.log(error))
